3567de6d2d0f99aee88d572cfc4e9e0c13f30865
shamnaraneesh123 <shamna989@gmail.com>
2021-03-25 01:57:12 -0700

    deleted stati and templates

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path
 from products_shop4u import views
 urlpatterns = [
-    path('',views.ok),
+    
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
 from django.http import httpResponse
 
-def ok(request);
-      return render(request,'ok.html')
